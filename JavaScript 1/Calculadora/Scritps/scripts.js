eb27b15eeb330eb1ac278988c87f21cc7cac2eb0
Hunkillerx <jdiazseg9611@universidadean.edu.co>
2018-03-08 22:03:52 -0500

    Calculadora trabajando falta completar documentacion

--- JavaScript 1/Calculadora/Scritps/scripts.js
+++ JavaScript 1/Calculadora/Scritps/scripts.js
@@ -1,6 +1,5 @@
 var pantalla = document.getElementById("pantalla");
-/*
-var historial = document.getElementById("historial");*/
+var historial = document.getElementById("historial");
 var btnNumeros = document.getElementsByClassName("btn-num");
 
 var numClick = function () {
@@ -18,72 +17,128 @@ for (var i = 0; i < btnNumeros.length; i++) {
 
 var valor1;
 var valor2;
-var operador;
+var operador = "";
+var resultado;
 
-document.getElementById("operator-+").onclick = function () {
-    if (operador === "+") {
+document.getElementById("operator-+").onclick = function suma() {
+    if (operador != "") {
         igual();
-    } else {
-        valor1 = pantalla.getAttribute("value");
-        pantalla.setAttribute("value", 0);
+        valor1 = resultado;
         operador = "+";
-
     }
+    valor1 = pantalla.getAttribute("value");
+    pantalla.setAttribute("value", 0);
+
+    operador = "+";
 
 };
 
-document.getElementById("operator--").onclick = function () {
+document.getElementById("operator--").onclick = function resta() {
+    if (operador != "") {
+        igual();
+        valor1 = resultado;
+        operador = "-";
+    }
     valor1 = pantalla.getAttribute("value");
     pantalla.setAttribute("value", 0);
     operador = "-";
 };
 
-document.getElementById("operator-*").onclick = function () {
+document.getElementById("operator-*").onclick = function multi() {
+    if (operador != "") {
+        igual();
+        valor1 = resultado;
+        operador = "*";
+    }
     valor1 = pantalla.getAttribute("value");
     pantalla.setAttribute("value", 0);
     operador = "*";
 };
 
-document.getElementById("operator-/").onclick = function () {
+document.getElementById("operator-/").onclick = function div() {
+    if (operador != "") {
+        igual();
+        valor1 = resultado;
+        operador = "/";
+    }
     valor1 = pantalla.getAttribute("value");
     pantalla.setAttribute("value", 0);
     operador = "/";
 };
 
 document.getElementById("operator-%").onclick = function () {
+    if (operador != "") {
+        igual();
+        valor1 = resultado;
+        operador = "%";
+    }
     valor1 = pantalla.getAttribute("value");
     pantalla.setAttribute("value", 0);
     operador = "%";
 };
 
 
-document.getElementById("operator-=").onclick = function igual() {
+document.getElementById("operator-=").onclick =
+    function igual() {
+        valor2 = pantalla.getAttribute("value");
+        switch (operador) {
+            case "+":
+                resultado = Number(valor1) + Number(valor2);
+                historial.append(valor1 + "+" + valor2 + "=" + resultado + " ")
+                break;
+            case "-":
+                resultado = Number(valor1) - Number(valor2);
+                historial.append(valor1 + "-" + valor2 + "=" + resultado + " ")
+                break;
+            case "*":
+                resultado = Number(valor1) * Number(valor2);
+                historial.append(valor1 + "*" + valor2 + "=" + resultado + " ")
+                break;
+            case "/":
+                resultado = Number(valor1) / Number(valor2);
+                historial.append(valor1 + "/" + valor2 + "=" + resultado + " ")
+                break;
+            case "%":
+                resultado = Number(valor1) * Number(valor2) / 100;
+                historial = Number(valor1).toString + "*" + Number(valor2).toString;
+                break;
+        };
+        operador = "";
+        pantalla.setAttribute("value", resultado);
+    }
+
+function igual() {
     valor2 = pantalla.getAttribute("value");
     switch (operador) {
         case "+":
             resultado = Number(valor1) + Number(valor2);
+            historial.append(valor1 + "+" + valor2 + "=" + resultado + " ")
+            pantalla.setAttribute("value", resultado);
             break;
         case "-":
             resultado = Number(valor1) - Number(valor2);
+            historial.append(valor1 + "-" + valor2 + "=" + resultado + " ")
             break;
         case "*":
             resultado = Number(valor1) * Number(valor2);
+            historial.append(valor1 + "*" + valor2 + "=" + resultado + " ")
             break;
         case "/":
             resultado = Number(valor1) / Number(valor2);
+            historial.append(valor1 + "/" + valor2 + "=" + resultado + " ")
             break;
         case "%":
             resultado = Number(valor1) * Number(valor2) / 100;
+            historial.append(valor1 + "%" + valor2 + "=" + resultado + " ")
             break;
     };
-
-
-pantalla.setAttribute("value", resultado);
-
+    pantalla.setAttribute("value", resultado);
+}
 
 document.getElementById("operator-c").onclick = function () {
-    memoria = 0;
+    operador = "";
+    valor1 = 0;
+    valor2 = 0;
+    resultado = 0;
     pantalla.setAttribute("value", 0);
 };
-    
-}
