b4078ca738a29637cef6ede8621181d96131cd2a
Hunkillerx <jdiazseg9611@universidadean.edu.co>
2018-03-29 19:09:04 -0500

    Reloj Finalizado

--- JavaScript 1/Reloj/js/main.js
+++ JavaScript 1/Reloj/js/main.js
@@ -1,12 +1,58 @@
-var meses = ['Enero', 'Febrero']
-var dias = ['Lunes']
+(function () {
 
-var actualizar = function(){
-    var miFecha = new Date();
-    document.getElementById('dia').innerHTML = dias[miFecha.getDay() - 1]+ '' + miFecha.getUTCDate() + 'de ';
-    document.getElementById('mes').innerHTML = meses[miFecha.getMonth]+ 'del';
-    document.getElementById('año').innerHTML = miFecha.getFullYear();
+        var actualizarHora = function () {
+            var fecha = new Date(),
+                horas = fecha.getHours(),
+                ampm,
+                minutos = fecha.getMinutes(),
+                segundos = fecha.getSeconds(),
+                diaSemana = fecha.getDay(),
+                dia = fecha.getDate(),
+                mes = fecha.getMonth(),
+                year = fecha.getFullYear();
+
+            var pHoras = document.getElementById('horas'),
+                pAMPM = document.getElementById('ampm'),
+                pMinutos = document.getElementById('minutos'),
+                pSegundos = document.getElementById('segundos'),
+                pDiaSemana = document.getElementById('diaSemana'),
+                pDia = document.getElementById('dia'),
+                pMes = document.getElementById('mes'),
+                pYear = document.getElementById('year');
+
+            var semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+
+            pDiaSemana.textContent = semana[diaSemana];
+
+            pDia.textContent = dia;
+
+            var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+            pMes.textContent = meses[mes];
+
+            pYear.textContent = year;
+
+            if (horas >= 12) {
+                horas = horas - 12;
+                ampm = 'PM';
+            } else {
+                ampm = 'AM';
+            }
+
+            if (horas == 0) {
+                horas == 12;
+            };
+            
+            pHoras.textContent = horas;
+            pAMPM.textContent = ampm;
+            
+            if(minutos < 10){ minutos = '0' + minutos};
+            if(segundos < 10){segundos = '0' + segundos};
+            pMinutos.textContent = minutos;
+            pSegundos.textContent = segundos;
+            
     
-    document.getElementById('hora').innerHTML = miFecha.getHours() + ' : ';
-    document.getElementById('minuto').innerHTML = miFecha.getMinutes() + ' : ';
-    document.getElementById('segundo').innerHTML = miFecha.getSeconds();
+        };
+    actualizarHora();
+    var intervalo = setInterval(actualizarHora, 1000);
+}())
