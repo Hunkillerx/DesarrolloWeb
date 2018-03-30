c65cb367e30d6647732ba98f2f7efe781b946a8c
jose <jdiazseg9611@universidadean.edu.co>
2018-03-29 19:45:33 -0500

    Init

--- ListaTareas/JS/Tarea.js
+++ ListaTareas/JS/Tarea.js
@@ -4,11 +4,19 @@ function Tarea(descripcion, fecha, realizada){
     this.realizada = realizada;
 
     this.generarHtmlTarea = function(pos){
+        var cadena = '<>'
         var cadena = '<input type="checkbox" name="task';
         cadena += pos+'" id="task'+pos+'" ';
         cadena += realizada?'checked':'';
         cadena += '><label for="task';
         cadena += pos +'">'+this.descripcion+'</label>';
-        return cadena;
+
+        var edit = document.createElement("button");
+        edit.type = "button";
+        edit.value = "Editar";
+        edit.onclick = func;
+        document.appendChild(edit);
+
+        return cadena,edit
     }
 }
